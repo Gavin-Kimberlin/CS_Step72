dd57e5ddc231712ee091425c64016573600e662d
Gavin Kimberlin <guviner@gmail.com>
2020-05-14 10:13:33 -0700

    Create a program that asks questions to the user.

--- CS_Step72/Program.cs
+++ CS_Step72/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CS_Step72
 {
@@ -10,6 +6,30 @@ namespace CS_Step72
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+
+            Console.WriteLine("What course are you on?");
+            string currentCourse = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            byte pageNumber = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Do you need help with anything? Please answer “true” or “false”.");
+            bool needHelp = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string positiveExperiences = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            byte hoursStudy = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.WriteLine("Your responses were:" + currentCourse + ", " + pageNumber + ", " + needHelp + ", " + positiveExperiences + ", " + feedback + ", " + hoursStudy);
+            Console.ReadLine();
         }
     }
 }
